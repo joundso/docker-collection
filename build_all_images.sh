01dc949c1b15dd62f5698b89c2a5df64f6cd5e7f
Jonathan Mang <56686638+joundso@users.noreply.github.com>
2021-07-30 13:51:12 +0200

    feat: added building scripts

---
+++ build_all_images.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+## Get version tag and registry-prefix from .env:
+source ./.env
+
+DIRECTORY=./dockerfiles
+
+for i in $DIRECTORY/*.dockerfile; do
+    ## Remove path:
+    IMAGE_NAME=${i##*/}
+    ## ... and file ending:
+    IMAGE_NAME="${IMAGE_NAME%.*}"
+
+    printf "\n\n##################################\n"
+    printf "Building $REGISTRY_PREFIX/$IMAGE_NAME:$ALPINE_VERSION_TAG"
+    printf "\n##################################\n"
+
+    printf "\n\nPlease insert your login credentials to registry: $REGISTRY_PREFIX ...\n"
+    docker login
+
+    printf "\nPulling cached $IMAGE_NAME image\n"
+    # pull latest image for caching:
+    docker pull $REGISTRY_PREFIX/$IMAGE_NAME
+    # build new image (latest):
+    docker build --progress=plain -f $i -t $REGISTRY_PREFIX/$IMAGE_NAME . 2>&1 | tee ./log_$IMAGE_NAME.log
+    printf "\n\nPushing $IMAGE_NAME image (latest)\n"
+    # push new image as new 'latest':
+    docker push "$REGISTRY_PREFIX/$IMAGE_NAME"
+    # also tag it with the new tag:
+    docker tag $REGISTRY_PREFIX/$IMAGE_NAME $REGISTRY_PREFIX/$IMAGE_NAME:$ALPINE_VERSION_TAG
+    # and also push this (tagged) image:
+    printf "\n\nPushing $IMAGE_NAME image ($ALPINE_VERSION_TAG)\n"
+    docker push "$REGISTRY_PREFIX/$IMAGE_NAME:$ALPINE_VERSION_TAG"
+done
--- readme.md
+++ readme.md
@@ -1,7 +1,17 @@
 # Docker Collection
 
-:bulb: Fork from <https://github.com/jackfusion/docker-collection>
+:bulb: Originally forked from <https://github.com/jackfusion/docker-collection>.
+
+## Building the images
+
+Run:
+
+```bash
+git clone https://github.com/joundso/docker-collection.git docker-collection
+cd docker-collection
+sh ./build_all_images.sh
+```
 
 ## Dockerfiles
 
-You can find all the Dockerfiles for the Automatic Builds from my Account on [Docker Hub](https://hub.docker.com/r/joundso/) in the  [dockerfiles](./dockerfiles/) folder.
+You can find all the Dockerfiles for the Automatic Builds from my Account on [Docker Hub](https://hub.docker.com/r/joundso/) in the [/dockerfiles](./dockerfiles/) folder.
